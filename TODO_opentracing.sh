0b0f3906b6c67e8aea96d7902c03accc01534b8a
Didier Donsez <didier.donsez@gmail.com>
2017-07-19 09:43:42 +0100

    Update TODO_opentracing.sh

--- TODO_opentracing.sh
+++ TODO_opentracing.sh
@@ -7,4 +7,6 @@
 
 open http://opentracing.io/documentation/
 
-docker run -d -p 5775:5775/udp -p 16686:16686 jaegertracing/all-in-one:latest
+INSTANCE_ID=1
+
+docker run --name opentracing-${INSTANCE_ID} -d -p 5775:5775/udp -p 16686:16686 jaegertracing/all-in-one:latest
